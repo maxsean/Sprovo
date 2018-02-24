d38d14158ff1cbe6b664d084ab27182c81390735
maxsean <xsma1990@gmail.com>
2018-02-24 18:06:24 -0500

    fixed devise routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
 
   scope :auth do
     get 'is_signed_in', to: 'auth#is_signed_in?'
   end
-  
+
   root 'static_pages#index'
 
   get '*path', to: 'static_pages#index'
