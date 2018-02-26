ed6077be00c169547485544e9e4ebe8781d1afea
maxsean <xsma1990@gmail.com>
2018-02-25 23:06:48 -0500

    added logic for sourcing images

---
+++ app/javascript/react/src/constants/railsAssetImagePath.js
@@ -0,0 +1,10 @@
+let railsAssetImagePath;
+if (process.env.NODE_ENV === 'development') {
+  railsAssetImagePath = filePath => `/assets/${filePath}`;
+}
+
+if (process.env.NODE_ENV === 'production') {
+  railsAssetImagePath = filePath => railsAssetHelper[filePath];
+}
+
+export { railsAssetImagePath }
