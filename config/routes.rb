0182b1bb6583ea8faab113bccfcdbabea8330f00
maxsean <xsma1990@gmail.com>
2018-02-28 14:01:01 -0500

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :update, :delete]
+      resources :images, only: [:update]
     end
   end
 
