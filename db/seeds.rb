809e4d497f3855591f560183f35ac5c37552404b
maxsean <xsma1990@gmail.com>
2018-03-02 16:06:24 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create!(first_name: "John", last_name: "Smith", handle: "jsmith", email: "john.smith@maily.com", password: "123456", role: "mentor")
+
+user2 = User.create!(first_name: "Jill", last_name: "Jones", handle: "jill.j", email: "jill.jones@maily.com", password: "654321", mentor_id: user1.id)
+
+user3 = User.create!(first_name: "Franklin", last_name: "Brown", handle: "b_frank", email: "frank_brown@gmail.com", password: "121212", mentor_id: user1.id)
+
+course1 = Course.create!(name: "American Literature")
+course2 = Course.create!(name: "Trigonometry")
+course3 = Course.create!(name: "American History")
+course4 = Course.create!(name: "Chemistry")
+course5 = Course.create!(name: "French Literature")
+course6 = Course.create!(name: "Algebra")
+course7 = Course.create!(name: "Asian History")
+course8 = Course.create!(name: "Biology")
+course9 = Course.create!(name: "British Literature")
+course10 = Course.create!(name: "Calculus")
+course11 = Course.create!(name: "European History")
+course12 = Course.create!(name: "Physics")
+
+grade1 = Grade.create!(user_id: user2.id, course_id: course1.id, year: "2016-2017", quarter: "3rd", score: "A")
+
+grade2 = Grade.create!(user_id: user2.id, course_id: course2.id, year: "2016-2017", quarter: "3rd", score: "C+")
+
+grade3 = Grade.create!(user_id: user2.id, course_id: course3.id, year: "2016-2017", quarter: "3rd", score: "B-")
+
+grade4 = Grade.create!(user_id: user2.id, course_id: course4.id, year: "2016-2017", quarter: "3rd", score: "B+")
+
+grade5 = Grade.create!(user_id: user2.id, course_id: course5.id, year: "2016-2017", quarter: "4th", score: "A")
+
+grade6 = Grade.create!(user_id: user2.id, course_id: course6.id, year: "2016-2017", quarter: "4th", score: "C+")
+
+grade7 = Grade.create!(user_id: user2.id, course_id: course7.id, year: "2016-2017", quarter: "4th", score: "B-")
+
+grade8 = Grade.create!(user_id: user2.id, course_id: course8.id, year: "2016-2017", quarter: "4th", score: "B+")
+
+grade9 = Grade.create!(user_id: user2.id, course_id: course9.id, year: "2017-2018", quarter: "1st", score: "A")
+
+grade10 = Grade.create!(user_id: user2.id, course_id: course10.id, year: "2017-2018", quarter: "1st", score: "C+")
+
+grade11 = Grade.create!(user_id: user2.id, course_id: course11.id, year: "2017-2018", quarter: "1st", score: "B-")
+
+grade12 = Grade.create!(user_id: user2.id, course_id: course12.id, year: "2017-2018", quarter: "1st", score: "B+")
+
+grade9 = Grade.create!(user_id: user3.id, course_id: course1.id, year: "2017-2018", quarter: "1st", score: "B")
+
+grade10 = Grade.create!(user_id: user3.id, course_id: course2.id, year: "2017-2018", quarter: "1st", score: "B+")
+
+grade11 = Grade.create!(user_id: user3.id, course_id: course3.id, year: "2017-2018", quarter: "1st", score: "A-")
+
+grade12 = Grade.create!(user_id: user3.id, course_id: course4.id, year: "2017-2018", quarter: "1st", score: "A+")
