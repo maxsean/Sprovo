b46b001502978011730d0a699ab5f2687087cad2
maxsean <xsma1990@gmail.com>
2018-03-05 13:26:53 -0500

    added phone and school to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303000117) do
+ActiveRecord::Schema.define(version: 20180303192931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 20180303000117) do
     t.integer "mentor_id"
     t.string "role", default: "student", null: false
     t.boolean "admin"
+    t.string "phone"
+    t.string "school"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["handle"], name: "index_users_on_handle", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
