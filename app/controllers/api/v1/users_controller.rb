12dd6af1e09c3b2effa4a994ee7f0eb8c1195eab
maxsean <xsma1990@gmail.com>
2018-03-05 13:28:21 -0500

    added functionality to update user basic information

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,27 @@ class Api::V1::UsersController < Api::V1::ApiController
 
   def update
     body = JSON.parse(request.body.read)
-
+    to_update = body["user"]
     user = User.find(params[:id])
-    user.update_attributes(bio: body["user"]["bio"])
+
+    if current_user && current_user == user
+      if to_update["bio"]
+        user.update_attributes(bio: to_update["bio"])
+
+        render json: {success: "Update Successful"}
+      elsif to_update["first_name"]
+        user.update_attributes(
+          first_name: to_update["first_name"],
+          last_name: to_update["last_name"],
+          phone: to_update["phone"],
+          school: to_update["school"],
+        )
+        
+        render json: {success: "Update Successful"}
+      end
+    else
+      render json: {error: "You do not have permission to edit this information"}
+    end
 
   end
 end
